3279f534508a3620b3898a028499fcddcd4cd421
atsu0203 <atsu0203@gmail.com>
2024-06-06 23:51:59 +0900

    globalstayleの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-// import { createGlobalStyle } from 'styled-components'
-import { Editor } from './pages/editor'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from "./pages/editor";
+
+const GlobalStyle = createGlobalStyle`
+body * {
+  box-sizing: border-box;
+}
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* <GlobalStyle /> */}
-        <Editor />
-      </header>
+      <GlobalStyle />
+      <Editor />
     </div>
   );
 }
