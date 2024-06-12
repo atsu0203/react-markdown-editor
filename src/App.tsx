038108b177e3331885a215b8318db09da5a8c5ae
atsu0203 <atsu0203@gmail.com>
2024-06-13 00:01:30 +0900

    テキストの閲覧、マークダウンのプレビューをバッググラウンドで生成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 import { Editor } from "./pages/editor";
+import { useStateWithStorage } from "./hooks/use_state_with_storage";
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import { History } from "./pages/history";
 
+const StorageKey = "/editor:text";
 const GlobalStyle = createGlobalStyle`
 body * {
   box-sizing: border-box;
@@ -11,11 +18,22 @@ body * {
 `;
 
 function App() {
+  const [text, setText] = useStateWithStorage("", StorageKey);
   return (
-    <div className="App">
+    <>
       <GlobalStyle />
-      <Editor />
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/editor">
+            <Editor text={text} setText={setText} />
+          </Route>
+          <Route exact path="/history">
+            <History setText={setText} />
+          </Route>
+          <Redirect to="/editor" path="*" />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/worker/convert_markdown_worker.ts
@@ -0,0 +1,15 @@
+import marked from "marked";
+import sanitizeHtml from "sanitize-html";
+/* eslint-disable-next-line no-restricted-globals */
+const worker: Worker = self as any;
+
+worker.addEventListener("message", (event) => {
+  
+  const text = event.data;
+  const html = sanitizeHtml(marked(text), {
+    allowedTags: [...sanitizeHtml.defaults.allowedTags, "h1", "h2"],
+  });
+  worker.postMessage({ html });
+});
+
+export {};
